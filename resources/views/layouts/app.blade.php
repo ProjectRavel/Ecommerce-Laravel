f223006b6190d3bd44358999dc3a7714eacf2b50
ProjectRavel <rafaelcodeid@gmail.com>
2025-01-29 18:44:21 +0700

    Create Admin Page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -264,15 +264,14 @@
     <div class="header-mobile header_sticky">
         <div class="container d-flex align-items-center h-100">
             <a class="mobile-nav-activator d-block position-relative" href="#">
-                <svg class="nav-icon" width="25" height="18" viewBox="0 0 25 18"
-                    xmlns="http://www.w3.org/2000/svg">
+                <svg class="nav-icon" width="25" height="18" viewBox="0 0 25 18" xmlns="http://www.w3.org/2000/svg">
                     <use href="#icon_nav" />
                 </svg>
                 <button class="btn-close-lg position-absolute top-0 start-0 w-100"></button>
             </a>
 
             <div class="logo">
-                <a href="index.html">
+                <a href="{{route('home.index')}}">
                     <img src="{{asset('assets/images/logo.png')}}" alt="Uomo" class="logo__image d-block" />
                 </a>
             </div>
@@ -291,8 +290,8 @@
             <div class="container">
                 <form action="#" method="GET" class="search-field position-relative mt-4 mb-3">
                     <div class="position-relative">
-                        <input class="search-field__input w-100 border rounded-1" type="text"
-                            name="search-keyword" placeholder="Search products" />
+                        <input class="search-field__input w-100 border rounded-1" type="text" name="search-keyword"
+                            placeholder="Search products" />
                         <button class="btn-icon search-popup__submit pb-0 me-2" type="submit">
                             <svg class="d-block" width="20" height="20" viewBox="0 0 20 20" fill="none"
                                 xmlns="http://www.w3.org/2000/svg">
@@ -312,7 +311,7 @@
                 <div class="overflow-hidden">
                     <ul class="navigation__list list-unstyled position-relative">
                         <li class="navigation__item">
-                            <a href="index.html" class="navigation__link">Home</a>
+                            <a href="{{route('home.index')}}" class="navigation__link">Home</a>
                         </li>
                         <li class="navigation__item">
                             <a href="shop.html" class="navigation__link">Shop</a>
@@ -332,8 +331,8 @@
 
             <div class="border-top mt-auto pb-2">
                 <div class="customer-links container mt-4 mb-2 pb-1">
-                    <svg class="d-inline-block align-middle" width="20" height="20" viewBox="0 0 20 20"
-                        fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg class="d-inline-block align-middle" width="20" height="20" viewBox="0 0 20 20" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
                         <use href="#icon_user" />
                     </svg>
                     <span class="d-inline-block ms-2 text-uppercase align-middle fw-medium">My Account</span>
@@ -360,8 +359,8 @@
                     </li>
                     <li>
                         <a href="#" class="footer__social-link d-block">
-                            <svg class="svg-icon svg-icon_instagram" width="14" height="13"
-                                viewBox="0 0 14 13" xmlns="http://www.w3.org/2000/svg">
+                            <svg class="svg-icon svg-icon_instagram" width="14" height="13" viewBox="0 0 14 13"
+                                xmlns="http://www.w3.org/2000/svg">
                                 <use href="#icon_instagram" />
                             </svg>
                         </a>
@@ -377,8 +376,8 @@
                     </li>
                     <li>
                         <a href="#" class="footer__social-link d-block">
-                            <svg class="svg-icon svg-icon_pinterest" width="14" height="15"
-                                viewBox="0 0 14 15" xmlns="http://www.w3.org/2000/svg">
+                            <svg class="svg-icon svg-icon_pinterest" width="14" height="15" viewBox="0 0 14 15"
+                                xmlns="http://www.w3.org/2000/svg">
                                 <use href="#icon_pinterest" />
                             </svg>
                         </a>
@@ -393,7 +392,7 @@
         <div class="container">
             <div class="header-desk header-desk_type_1">
                 <div class="logo">
-                    <a href="index.html">
+                    <a href="{{route('home.index')}}">
                         <img src="{{asset('assets/images/logo.png')}}" alt="Uomo" class="logo__image d-block" />
                     </a>
                 </div>
@@ -401,7 +400,7 @@
                 <nav class="navigation">
                     <ul class="navigation__list list-unstyled d-flex">
                         <li class="navigation__item">
-                            <a href="index.html" class="navigation__link">Home</a>
+                            <a href="{{route('home.index')}}" class="navigation__link">Home</a>
                         </li>
                         <li class="navigation__item">
                             <a href="shop.html" class="navigation__link">Shop</a>
@@ -422,8 +421,8 @@
                     <div class="header-tools__item hover-container">
                         <div class="js-hover__open position-relative">
                             <a class="js-search-popup search-field__actor" href="#">
-                                <svg class="d-block" width="20" height="20" viewBox="0 0 20 20"
-                                    fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <svg class="d-block" width="20" height="20" viewBox="0 0 20 20" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
                                     <use href="#icon_search" />
                                 </svg>
                                 <i class="btn-icon btn-close-lg"></i>
@@ -434,11 +433,11 @@
                             <form action="#" method="GET" class="search-field container">
                                 <p class="text-uppercase text-secondary fw-medium mb-4">What are you looking for?</p>
                                 <div class="position-relative">
-                                    <input class="search-field__input search-popup__input w-100 fw-medium"
-                                        type="text" name="search-keyword" placeholder="Search products" />
+                                    <input class="search-field__input search-popup__input w-100 fw-medium" type="text"
+                                        name="search-keyword" placeholder="Search products" />
                                     <button class="btn-icon search-popup__submit" type="submit">
-                                        <svg class="d-block" width="20" height="20" viewBox="0 0 20 20"
-                                            fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <svg class="d-block" width="20" height="20" viewBox="0 0 20 20" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
                                             <use href="#icon_search" />
                                         </svg>
                                     </button>
@@ -470,18 +469,30 @@
                         </div>
                     </div>
 
-                    <div class="header-tools__item hover-container">
-                        <a href="login.html" class="header-tools__item">
-                            <svg class="d-block" width="20" height="20" viewBox="0 0 20 20" fill="none"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <use href="#icon_user" />
-                            </svg>
-                        </a>
-                    </div>
+                    @guest
+                        <div class="header-tools__item hover-container">
+                            <a href="{{route('login')}}" class="header-tools__item">
+                                <svg class="d-block" width="20" height="20" viewBox="0 0 20 20" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <use href="#icon_user" />
+                                </svg>
+                            </a>
+                        </div>
+                    @else
+                        <div class="header-tools__item hover-container">
+                            <a href="{{Auth::user()->utype === 'ADM' ? route('admin.index') : route('user.index')}}"
+                                class="header-tools__item">
+                                <span>{{Auth::user()->name}}</span>
+                                <svg class="d-block" width="20" height="20" viewBox="0 0 20 20" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <use href="#icon_user" />
+                                </svg>
+                            </a>
+                        </div>
+                    @endguest
 
                     <a href="wishlist.html" class="header-tools__item">
-                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none"
-                            xmlns="http://www.w3.org/2000/svg">
+                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <use href="#icon_heart" />
                         </svg>
                     </a>
@@ -506,8 +517,9 @@
             <div class="row row-cols-lg-5 row-cols-2">
                 <div class="footer-column footer-store-info col-12 mb-4 mb-lg-0">
                     <div class="logo">
-                        <a href="index.html">
-                            <img src="{{asset('assets/images/logo.png')}}" alt="SurfsideMedia" class="logo__image d-block" />
+                        <a href="{{route('home.index')}}">
+                            <img src="{{asset('assets/images/logo.png')}}" alt="SurfsideMedia"
+                                class="logo__image d-block" />
                         </a>
                     </div>
                     <p class="footer-address">123 Beach Avenue, Surfside City, CA 00000</p>
@@ -517,32 +529,32 @@
                     <ul class="social-links list-unstyled d-flex flex-wrap mb-0">
                         <li>
                             <a href="#" class="footer__social-link d-block">
-                                <svg class="svg-icon svg-icon_facebook" width="9" height="15"
-                                    viewBox="0 0 9 15" xmlns="http://www.w3.org/2000/svg">
+                                <svg class="svg-icon svg-icon_facebook" width="9" height="15" viewBox="0 0 9 15"
+                                    xmlns="http://www.w3.org/2000/svg">
                                     <use href="#icon_facebook" />
                                 </svg>
                             </a>
                         </li>
                         <li>
                             <a href="#" class="footer__social-link d-block">
-                                <svg class="svg-icon svg-icon_twitter" width="14" height="13"
-                                    viewBox="0 0 14 13" xmlns="http://www.w3.org/2000/svg">
+                                <svg class="svg-icon svg-icon_twitter" width="14" height="13" viewBox="0 0 14 13"
+                                    xmlns="http://www.w3.org/2000/svg">
                                     <use href="#icon_twitter" />
                                 </svg>
                             </a>
                         </li>
                         <li>
                             <a href="#" class="footer__social-link d-block">
-                                <svg class="svg-icon svg-icon_instagram" width="14" height="13"
-                                    viewBox="0 0 14 13" xmlns="http://www.w3.org/2000/svg">
+                                <svg class="svg-icon svg-icon_instagram" width="14" height="13" viewBox="0 0 14 13"
+                                    xmlns="http://www.w3.org/2000/svg">
                                     <use href="#icon_instagram" />
                                 </svg>
                             </a>
                         </li>
                         <li>
                             <a href="#" class="footer__social-link d-block">
-                                <svg class="svg-icon svg-icon_youtube" width="16" height="11"
-                                    viewBox="0 0 16 11" xmlns="http://www.w3.org/2000/svg">
+                                <svg class="svg-icon svg-icon_youtube" width="16" height="11" viewBox="0 0 16 11"
+                                    xmlns="http://www.w3.org/2000/svg">
                                     <path
                                         d="M15.0117 1.8584C14.8477 1.20215 14.3281 0.682617 13.6992 0.518555C12.5234 0.19043 7.875 0.19043 7.875 0.19043C7.875 0.19043 3.19922 0.19043 2.02344 0.518555C1.39453 0.682617 0.875 1.20215 0.710938 1.8584C0.382812 3.00684 0.382812 5.46777 0.382812 5.46777C0.382812 5.46777 0.382812 7.90137 0.710938 9.07715C0.875 9.7334 1.39453 10.2256 2.02344 10.3896C3.19922 10.6904 7.875 10.6904 7.875 10.6904C7.875 10.6904 12.5234 10.6904 13.6992 10.3896C14.3281 10.2256 14.8477 9.7334 15.0117 9.07715C15.3398 7.90137 15.3398 5.46777 15.3398 5.46777C15.3398 5.46777 15.3398 3.00684 15.0117 1.8584ZM6.34375 7.68262V3.25293L10.2266 5.46777L6.34375 7.68262Z" />
                                 </svg>
@@ -550,8 +562,8 @@
                         </li>
                         <li>
                             <a href="#" class="footer__social-link d-block">
-                                <svg class="svg-icon svg-icon_pinterest" width="14" height="15"
-                                    viewBox="0 0 14 15" xmlns="http://www.w3.org/2000/svg">
+                                <svg class="svg-icon svg-icon_pinterest" width="14" height="15" viewBox="0 0 14 15"
+                                    xmlns="http://www.w3.org/2000/svg">
                                     <use href="#icon_pinterest" />
                                 </svg>
                             </a>
@@ -566,12 +578,11 @@
                                 Us</a></li>
                         <li class="sub-menu__item"><a href="#" class="menu-link menu-link_us-s">Careers</a>
                         </li>
-                        <li class="sub-menu__item"><a href="#"
-                                class="menu-link menu-link_us-s">Affiliates</a></li>
-                        <li class="sub-menu__item"><a href="blog_list1.html"
-                                class="menu-link menu-link_us-s">Blog</a></li>
-                        <li class="sub-menu__item"><a href="contact-2.html"
-                                class="menu-link menu-link_us-s">Contact Us</a></li>
+                        <li class="sub-menu__item"><a href="#" class="menu-link menu-link_us-s">Affiliates</a></li>
+                        <li class="sub-menu__item"><a href="blog_list1.html" class="menu-link menu-link_us-s">Blog</a>
+                        </li>
+                        <li class="sub-menu__item"><a href="contact-2.html" class="menu-link menu-link_us-s">Contact
+                                Us</a></li>
                     </ul>
                 </div>
 
@@ -580,8 +591,8 @@
                     <ul class="sub-menu__list list-unstyled">
                         <li class="sub-menu__item"><a href="shop2.html" class="menu-link menu-link_us-s">New
                                 Arrivals</a></li>
-                        <li class="sub-menu__item"><a href="shop3.html"
-                                class="menu-link menu-link_us-s">Accessories</a></li>
+                        <li class="sub-menu__item"><a href="shop3.html" class="menu-link menu-link_us-s">Accessories</a>
+                        </li>
                         <li class="sub-menu__item"><a href="shop4.html" class="menu-link menu-link_us-s">Men</a>
                         </li>
                         <li class="sub-menu__item"><a href="shop5.html" class="menu-link menu-link_us-s">Women</a>
@@ -596,11 +607,11 @@
                     <ul class="sub-menu__list list-unstyled">
                         <li class="sub-menu__item"><a href="#" class="menu-link menu-link_us-s">Customer
                                 Service</a></li>
-                        <li class="sub-menu__item"><a href="account_dashboard.html"
-                                class="menu-link menu-link_us-s">My Account</a>
+                        <li class="sub-menu__item"><a href="account_dashboard.html" class="menu-link menu-link_us-s">My
+                                Account</a>
                         </li>
-                        <li class="sub-menu__item"><a href="store_location.html"
-                                class="menu-link menu-link_us-s">Find a Store</a>
+                        <li class="sub-menu__item"><a href="store_location.html" class="menu-link menu-link_us-s">Find a
+                                Store</a>
                         </li>
                         <li class="sub-menu__item"><a href="#" class="menu-link menu-link_us-s">Legal &
                                 Privacy</a></li>
@@ -631,8 +642,8 @@
             <div class="container d-md-flex align-items-center">
                 <span class="footer-copyright me-auto">©2024 Surfside Media</span>
                 <div class="footer-settings d-md-flex align-items-center">
-                    <a href="privacy-policy.html">Privacy Policy</a> &nbsp;|&nbsp; <a
-                        href="terms-conditions.html">Terms &amp;
+                    <a href="privacy-policy.html">Privacy Policy</a> &nbsp;|&nbsp; <a href="terms-conditions.html">Terms
+                        &amp;
                         Conditions</a>
                 </div>
             </div>
@@ -643,7 +654,7 @@
     <footer class="footer-mobile container w-100 px-5 d-md-none bg-body">
         <div class="row text-center">
             <div class="col-4">
-                <a href="index.html" class="footer-mobile__link d-flex flex-column align-items-center">
+                <a href="{{route('home.index')}}" class="footer-mobile__link d-flex flex-column align-items-center">
                     <svg class="d-block" width="18" height="18" viewBox="0 0 18 18" fill="none"
                         xmlns="http://www.w3.org/2000/svg">
                         <use href="#icon_home" />
@@ -653,7 +664,7 @@
             </div>
 
             <div class="col-4">
-                <a href="index.html" class="footer-mobile__link d-flex flex-column align-items-center">
+                <a href="{{route('home.index')}}" class="footer-mobile__link d-flex flex-column align-items-center">
                     <svg class="d-block" width="18" height="18" viewBox="0 0 18 18" fill="none"
                         xmlns="http://www.w3.org/2000/svg">
                         <use href="#icon_hanger" />
@@ -663,7 +674,7 @@
             </div>
 
             <div class="col-4">
-                <a href="index.html" class="footer-mobile__link d-flex flex-column align-items-center">
+                <a href="{{route('home.index')}}" class="footer-mobile__link d-flex flex-column align-items-center">
                     <div class="position-relative">
                         <svg class="d-block" width="18" height="18" viewBox="0 0 20 20" fill="none"
                             xmlns="http://www.w3.org/2000/svg">
