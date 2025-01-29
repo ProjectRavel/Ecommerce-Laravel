3d579736582f3005e3ec4550f0295f4d5d84dc18
ProjectRavel <rafaelcodeid@gmail.com>
2025-01-29 15:45:28 +0700

    Update UI & User Auth

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,15 +10,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <meta name="author" content="surfside media" />
-    <link rel="shortcut icon" href="{{asset(assets/images/favicon.ico) }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{asset('assets/images/favicon.ico') }}" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.gstatic.com/">
     <link
         href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
         rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Allura&amp;display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{asset(assets/css/plugins/swiper.min.css)}}" type="text/css" />
-    <link rel="stylesheet" href="{{asset(assets/css/style.css)}}" type="text/css" />
-    <link rel="stylesheet" href="{{asset(assets/css/custom.css)}}" type="text/css" />
+    <link rel="stylesheet" href="{{asset('assets/css/plugins/swiper.min.css')}}" type="text/css" />
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}" type="text/css" />
+    <link rel="stylesheet" href="{{asset('assets/css/custom.css')}}" type="text/css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
         integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw=="
         crossorigin="anonymous" referrerpolicy="no-referrer">
@@ -273,7 +273,7 @@
 
             <div class="logo">
                 <a href="index.html">
-                    <img src="{{asset(assets/images/logo.png)}}" alt="Uomo" class="logo__image d-block" />
+                    <img src="{{asset('assets/images/logo.png')}}" alt="Uomo" class="logo__image d-block" />
                 </a>
             </div>
 
@@ -394,7 +394,7 @@
             <div class="header-desk header-desk_type_1">
                 <div class="logo">
                     <a href="index.html">
-                        <img src="{{asset(assets/images/logo.png)}}" alt="Uomo" class="logo__image d-block" />
+                        <img src="{{asset('assets/images/logo.png')}}" alt="Uomo" class="logo__image d-block" />
                     </a>
                 </div>
 
@@ -507,7 +507,7 @@
                 <div class="footer-column footer-store-info col-12 mb-4 mb-lg-0">
                     <div class="logo">
                         <a href="index.html">
-                            <img src="{{asset(assets/images/logo.png)}}" alt="SurfsideMedia" class="logo__image d-block" />
+                            <img src="{{asset('assets/images/logo.png')}}" alt="SurfsideMedia" class="logo__image d-block" />
                         </a>
                     </div>
                     <p class="footer-address">123 Beach Avenue, Surfside City, CA 00000</p>
@@ -680,12 +680,12 @@
     <div id="scrollTop" class="visually-hidden end-0"></div>
     <div class="page-overlay"></div>
 
-    <script src="{{asset(assets/js/plugins/jquery.min.js) }}"></script>
-    <script src="{{asset(assets/js/plugins/bootstrap.bundle.min.js) }}"></script>
-    <script src="{{asset(assets/js/plugins/bootstrap-slider.min.js) }}"></script>
-    <script src="{{asset(assets/js/plugins/swiper.min.js) }}"></script>
-    <script src="{{asset(assets/js/plugins/countdown.js)}}"></script>
-    <script src="{{asset(assets/js/theme.js)}}"></script>
+    <script src="{{asset('assets/js/plugins/jquery.min.js') }}"></script>
+    <script src="{{asset('assets/js/plugins/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{asset('assets/js/plugins/bootstrap-slider.min.js') }}"></script>
+    <script src="{{asset('assets/js/plugins/swiper.min.js') }}"></script>
+    <script src="{{asset('assets/js/plugins/countdown.js')}}"></script>
+    <script src="{{asset('assets/js/theme.js')}}"></script>
     @stack("scripts")
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home.index');
