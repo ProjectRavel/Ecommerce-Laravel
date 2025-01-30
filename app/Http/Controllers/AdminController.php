de4c83be6bc7af9dd36f4631b07b376a8c339183
ProjectRavel <rafaelcodeid@gmail.com>
2025-01-30 23:15:02 +0700

    make table brand &  model brand

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brand;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -9,4 +10,9 @@ class AdminController extends Controller
     public function index(){
         return view("admin.index");
     }
+
+   public function brands(Brand $brand){
+    $brands = $brand->orderBy("id","desc")->paginate(15);
+    return view('admin.brands', compact('brands'));
+   }
 }
