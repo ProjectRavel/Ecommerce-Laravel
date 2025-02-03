47506f1b22f8461e88c89ea1d62c7287bd3f5a4e
ProjectRavel <rafaelcodeid@gmail.com>
2025-02-04 01:07:48 +0700

    add brand edit feature

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,25 +6,30 @@ use App\Models\Brand;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\File;
 use Intervention\Image\Facades\Image;
 
 class AdminController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view("admin.index");
     }
 
-   public function brands(Brand $brand){
-    $brands = $brand->orderBy("id","desc")->paginate(15);
-    return view('admin.brands', compact('brands'));
-   }
+    public function brands(Brand $brand)
+    {
+        $brands = $brand->orderBy("id", "desc")->paginate(15);
+        return view('admin.brands', compact('brands'));
+    }
 
-   public function add_brand(){
-    return view("admin.brand-add");
-   }
+    public function add_brand()
+    {
+        return view("admin.brand-add");
+    }
 
-   public function brand_store(Request $request){
-   
+    public function brand_store(Request $request)
+    {
+
         // Validasi input
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
@@ -54,15 +59,54 @@ class AdminController extends Controller
 
         // Mengalihkan dengan pesan sukses
         return redirect()->route('admin.brands')->with('status', 'Brand has been added successfully!');
-   }
+    }
 
-   public function GenerateBrandThumbnailsImage($imagePath, $imageName){
+    public function brand_edit($id)
+    {
+        $brand = Brand::find($id);
+        return view('admin.brand-edit', compact('brand'));
+    }
+
+    public function brand_update(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'slug' => ['required', 'unique:brands,slug'],
+            'image' => ['mimes:jpeg,jpg,png', 'max:2048']
+        ]);
+
+        $brand = Brand::find($request->id);
+        $brand->name = $request->name;
+        $brand->slug = Str::slug($request->name);
+        if ($request->hasFile('image')) {
+
+            if (File::exists(public_path('uploads/brands/' . $brand->image))) {
+                File::delete(public_path('uploads/brands/' . $brand->image));
+            }
+            $image = $request->file('image'); // Mengambil file gambar dari request
+            $file_extension = $image->extension(); // Mendapatkan ekstensi file gambar
+            $file_name = Carbon::now()->timestamp . '.' . $file_extension; // Membuat nama file unik
+            $image->move(public_path('uploads/brands'), $file_name); // Memindahkan file gambar ke direktori yang ditentukan
+            $brand->image = $file_name; // Menyimpan nama file di atribut image dari objek Brand
+
+            // Membuat thumbnail
+            $this->GenerateBrandThumbnailsImage(public_path('uploads/brands') . '/' . $file_name, $file_name);
+        }
+
+        // Menyimpan Brand ke database
+        $brand->save();
+
+        // Mengalihkan dengan pesan sukses
+        return redirect()->route('admin.brands')->with('status', 'Brand has been edited!');
+    }
+
+    public function GenerateBrandThumbnailsImage($imagePath, $imageName)
+    {
         $destinationPath = public_path('uploads/brands'); // Path tujuan
         $img = Image::make($imagePath); // Membaca gambar yang diunggah
         $img->fit(124, 124, function ($constraint) { // Mengubah ukuran dan memotong gambar
             $constraint->upsize();
         });
         $img->save($destinationPath . '/' . $imageName); // Menyimpan gambar yang telah diubah ukurannya
-   }
-
+    }
 }
